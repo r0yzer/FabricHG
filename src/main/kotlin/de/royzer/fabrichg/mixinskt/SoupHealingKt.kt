2cea4af0dd3adc67871b6547a68dc6a113504ff2
Wxffel <66750759+Wxffel@users.noreply.github.com>
2022-01-09 03:55:38 +0100

    Added precise soup usage

--- src/main/kotlin/de/royzer/fabrichg/mixinskt/SoupHealingKt.kt
+++ src/main/kotlin/de/royzer/fabrichg/mixinskt/SoupHealingKt.kt
@@ -1,24 +1,49 @@
 package de.royzer.fabrichg.mixinskt
 
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+import kotlin.math.min
 
 object SoupHealingKt {
-    @JvmStatic
-    fun onSoupUse(player: PlayerEntity, stack: ItemStack, cir: CallbackInfoReturnable<TypedActionResult<ItemStack>>, world: World, hand: Hand) {
-        if (player.health >= player.maxHealth) {
-            if (player.hungerManager.isNotFull) {
-                player.hungerManager.add(6, 1F)
-                cir.returnValue = TypedActionResult.pass(ItemStack(Items.BOWL))
-            }
-            return
+    fun onPotentialSoupUse (
+        player: PlayerEntity, item: Item,
+        cir: CallbackInfoReturnable<TypedActionResult<ItemStack>>,
+        world: World, hand: Hand
+    ) {
+        val hungerManager = player.hungerManager
+
+        if (!item.isStew || player.health >= player.maxHealth && !hungerManager.isNotFull) return
+
+        var consumedSoup = false
+
+        if (player.health < player.maxHealth) {
+            player.health = min(player.health + 7, player.maxHealth)
+            consumedSoup = true
+        } else if (hungerManager.isNotFull) {
+            hungerManager.add(item.restoredFood, item.restoredSaturation)
+            consumedSoup = true
         }
-        player.heal(7F)
-        cir.returnValue = TypedActionResult.pass(ItemStack(Items.BOWL))
+
+        if (consumedSoup) cir.returnValue = TypedActionResult.pass(ItemStack(Items.BOWL))
     }
+
+    private val Item.isStew: Boolean
+        get() = when (this) {
+            Items.MUSHROOM_STEW -> true
+            Items.BEETROOT_SOUP -> true
+            Items.RABBIT_STEW -> true
+            else -> false
+        }
+
+    private val Item.restoredFood: Int
+        get() = this.foodComponent?.hunger ?: 0
+
+    private val Item.restoredSaturation: Float
+        get() = this.foodComponent?.saturationModifier ?: 0f
 }
