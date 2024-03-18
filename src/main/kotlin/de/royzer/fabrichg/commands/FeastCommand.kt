3024b799a3c047e63fc0086d4431d577483a7954
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-03-18 20:44:26 +0100

    add time to feast command

--- src/main/kotlin/de/royzer/fabrichg/commands/FeastCommand.kt
+++ src/main/kotlin/de/royzer/fabrichg/commands/FeastCommand.kt
@@ -6,8 +6,16 @@ import net.silkmc.silk.commands.command
 val feastCommand = command("feast") {
     literal("start") {
         requiresPermissionLevel(4)
+
         runs {
             Feast.start()
+        }
+
+        argument<Int>("time") { timeLeft ->
+            runs {
+                Feast.timeLeft = timeLeft()
+                Feast.start()
+            }
         }
     }
 }
