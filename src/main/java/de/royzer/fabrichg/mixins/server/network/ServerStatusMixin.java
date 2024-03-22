cb159c3cac06c3daf3692dd1927d55bbb98b4df9
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-03-22 22:11:42 +0100

    cleanup merkel mixin

--- src/main/java/de/royzer/fabrichg/mixins/server/network/ServerStatusMixin.java
+++ src/main/java/de/royzer/fabrichg/mixins/server/network/ServerStatusMixin.java
@@ -4,6 +4,7 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.status.ServerStatus;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,6 +15,11 @@ import java.util.UUID;
 
 @Mixin(ServerStatus.class)
 class ServerStatusMixin {
+    @Unique
+    private GameProfile text(String text) {
+        return new GameProfile(UUID.randomUUID(), text);
+    }
+
     @Inject(
             method = "<init>",
             at = @At("TAIL")
@@ -26,19 +32,22 @@ class ServerStatusMixin {
             boolean secureChat,
             CallbackInfo ci
     ) {
-        ServerStatus.Version serverVersion = version.orElse(new ServerStatus.Version("brain", 187));
         players.ifPresent((playerList) -> {
+            ServerStatus.Version serverVersion = version.orElse(new ServerStatus.Version("brain", 187));
             List<GameProfile> playersSample = playerList.sample();
             List<GameProfile> playersCopy = List.copyOf(playersSample);
+
             playersSample.clear();
-            playersSample.add(new GameProfile(UUID.randomUUID(), " §b Fabric HG Server"));
-            playersSample.add(new GameProfile(UUID.randomUUID(), " §2 in der " + serverVersion.name()));
-            playersSample.add(new GameProfile(UUID.randomUUID(), " §b currently under §4Development"));
+
+            playersSample.add(text(" §b Fabric HG"));
+            playersSample.add(text(" §2 in der " + serverVersion.name()));
+            playersSample.add(text(" §b currently under §4Development"));
+
             if (playersCopy.size() < 5 && !playersCopy.isEmpty()) {
-                playersSample.add(new GameProfile(UUID.randomUUID(), " §e Diese " + playersCopy.size() + " Spieler sind online!"));
+                playersSample.add(text(" §e Diese " + playersCopy.size() + " Spieler sind online!"));
                 playersCopy.forEach((onlinePlayer) -> playersSample.add(new GameProfile(UUID.randomUUID(), " - §5 " + onlinePlayer.getName())));
             } else {
-                playersSample.add(new GameProfile(UUID.randomUUID(), " §e Aktuell " + playersCopy.size() + " Spieler online!"));
+                playersSample.add(text(" §e Aktuell " + playersCopy.size() + " Spieler online!"));
             }
         });
     }
