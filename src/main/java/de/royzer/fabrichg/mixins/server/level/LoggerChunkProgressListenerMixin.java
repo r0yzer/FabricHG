350336d82f1064d4815d1926ffbdf58f5b6919b1
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-03-24 17:09:06 +0100

    fix progress > 100%

--- src/main/java/de/royzer/fabrichg/mixins/server/level/LoggerChunkProgressListenerMixin.java
+++ src/main/java/de/royzer/fabrichg/mixins/server/level/LoggerChunkProgressListenerMixin.java
@@ -79,6 +79,7 @@ class LoggerChunkProgressListenerMixin {
     )
     public void changeMotd(ChunkPos chunkPosition, ChunkStatus newStatus, CallbackInfo ci) {
         int progress = this.getProgress();
+        if (progress > 100) return;
         int r = (int) lerp(0, 255, ((float) 100-progress) / 100);
         int g = (int) lerp(0, 255, ((float) progress) / 100);
         int b = 0;
