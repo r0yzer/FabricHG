a7fe443e8a401e54e170578dc6dc0e4e0437fd9c
Hotkeyyy <64286456+Hotkeyyy@users.noreply.github.com>
2024-03-24 16:18:49 +0100

    hgbot in invincibility nicht hauen

--- src/main/kotlin/de/royzer/fabrichg/mixinskt/LivingEntityMixinKt.kt
+++ src/main/kotlin/de/royzer/fabrichg/mixinskt/LivingEntityMixinKt.kt
@@ -1,5 +1,6 @@
 package de.royzer.fabrichg.mixinskt
 
+import de.royzer.fabrichg.bots.HGBot
 import de.royzer.fabrichg.game.GamePhaseManager
 import de.royzer.fabrichg.game.phase.PhaseType
 import net.minecraft.server.level.ServerPlayer
@@ -9,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
 object LivingEntityMixinKt {
     fun onDamage(source: DamageSource, amount: Float, entity: LivingEntity, cir: CallbackInfoReturnable<Boolean>) {
-        if (entity is ServerPlayer) {
+        if (entity is ServerPlayer || entity is HGBot) {
             if (GamePhaseManager.currentPhaseType != PhaseType.INGAME) cir.returnValue = false
         } else if (GamePhaseManager.currentPhaseType == PhaseType.LOBBY || GamePhaseManager.currentPhaseType == PhaseType.END)
             cir.returnValue = false
