163930fab97602530329ca3722f30e0d04ef091b
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-03-27 12:25:06 +0100

    update kits with more properties

--- src/main/kotlin/de/royzer/fabrichg/kit/kits/GravityKit.kt
+++ src/main/kotlin/de/royzer/fabrichg/kit/kits/GravityKit.kt
@@ -14,6 +14,9 @@ val gravityKit = kit("Gravity") {
     val maxUsesHit by property(3, "max uses (hit)")
     val maxUsesClick by property(1, "max uses (click)")
 
+    val hitDuration by property(75, "levitation duration (hit)")
+    val clickDuration by property(300, "levitation duration (click)")
+
     kitSelectorItem = Items.PURPLE_GLAZED_TERRACOTTA.defaultInstance
 
     description = "Send yourself or others in the sky"
@@ -21,14 +24,14 @@ val gravityKit = kit("Gravity") {
     kitItem {
         itemStack = kitSelectorItem
         onClick { hgPlayer, _ ->
-            hgPlayer.serverPlayer?.addEffect(MobEffectInstance(MobEffects.LEVITATION, 300, 0, false, false))
+            hgPlayer.serverPlayer?.addEffect(MobEffectInstance(MobEffects.LEVITATION, clickDuration, 0, false, false))
 
             hgPlayer.checkUsesForCooldown(kit, maxUsesClick)
         }
 
         onHitPlayer { hgPlayer, kit, clickedPlayer ->
             if (clickedPlayer.hgPlayer.isNeo) return@onHitPlayer
-            clickedPlayer.addEffect(MobEffectInstance(MobEffects.LEVITATION, 75, 0, false, false))
+            clickedPlayer.addEffect(MobEffectInstance(MobEffects.LEVITATION, hitDuration, 0, false, false))
 
             hgPlayer.checkUsesForCooldown(kit, maxUsesHit)
         }
--- src/main/kotlin/de/royzer/fabrichg/kit/kits/SnailKit.kt
+++ src/main/kotlin/de/royzer/fabrichg/kit/kits/SnailKit.kt
@@ -2,6 +2,7 @@ package de.royzer.fabrichg.kit.kits
 
 import de.royzer.fabrichg.data.hgplayer.hgPlayer
 import de.royzer.fabrichg.kit.kit
+import de.royzer.fabrichg.kit.property.property
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.item.Items
@@ -13,9 +14,11 @@ val snailKit = kit("Snail") {
     usableInInvincibility = false
     description = "Give your enemies slowness when attacking them"
 
+    val maxInt by property(4, "max int (0 = always slow)")
+
     kitEvents {
         onHitPlayer { _, _, target ->
-            if (!target.hgPlayer.isNeo && Random.nextInt(5) == 3)
+            if (!target.hgPlayer.isNeo && Random.nextInt(maxInt) == 0)
                 target.addEffect(MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 25, 1))
         }
     }
