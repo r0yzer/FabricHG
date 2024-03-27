114ed5bb4aab2089ac3c0b8af95e08ad818f8542
Hotkeyyy <64286456+Hotkeyyy@users.noreply.github.com>
2024-03-27 20:57:29 +0100

    added equipmentpackets to FakeServerPlayer

--- src/main/kotlin/de/royzer/fabrichg/bots/FakeServerPlayer.kt
+++ src/main/kotlin/de/royzer/fabrichg/bots/FakeServerPlayer.kt
@@ -1,14 +1,22 @@
 package de.royzer.fabrichg.bots
 
 import com.mojang.authlib.GameProfile
+import com.mojang.datafixers.util.Pair
 import de.royzer.fabrichg.data.hgplayer.hgPlayer
 import de.royzer.fabrichg.kit.randomKit
+import de.royzer.fabrichg.mixins.server.level.ChunkMapAccessor
 import de.royzer.fabrichg.server
 import de.royzer.fabrichg.settings.ConfigManager
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup
+import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket
 import net.minecraft.server.level.ClientInformation
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 
 class FakeServerPlayer(gameProfile: GameProfile) : ServerPlayer(server, server.overworld(), gameProfile, ClientInformation.createDefault() ) {
 
@@ -46,5 +54,20 @@ class FakeServerPlayer(gameProfile: GameProfile) : ServerPlayer(server, server.o
             hgBot.die(damageSource)
         }
     }
+
+    override fun setItemSlot(slot: EquipmentSlot, stack: ItemStack) {
+        super.setItemSlot(slot, stack)
+        sendUpdateEquipmentPackets()
+    }
+
+    private fun sendUpdateEquipmentPackets(){
+        val list: java.util.ArrayList<Pair<EquipmentSlot, ItemStack>> =
+            ArrayList()
+        EquipmentSlot.entries.forEach {
+            list.add(Pair(it, getItemBySlot(it) ?: Items.AIR.defaultInstance!!))
+        }
+        server.playerList.broadcastAll(ClientboundSetEquipmentPacket(id, list))
+    }
+
 
 }
