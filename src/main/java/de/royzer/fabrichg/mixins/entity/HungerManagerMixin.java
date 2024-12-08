030c36e89be0dd77200acafa098ffe04588e2c51
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-12-08 22:11:04 +0100

    hunger weniger
    15% hunge3r oder so

--- src/main/java/de/royzer/fabrichg/mixins/entity/HungerManagerMixin.java
+++ src/main/java/de/royzer/fabrichg/mixins/entity/HungerManagerMixin.java
@@ -2,10 +2,12 @@ package de.royzer.fabrichg.mixins.entity;
 
 import de.royzer.fabrichg.game.GamePhaseManager;
 import de.royzer.fabrichg.game.phase.PhaseType;
+import kotlin.random.Random;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.food.FoodProperties;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,6 +25,10 @@ public class HungerManagerMixin {
 //        }
 //    }
 
+    @Shadow private int lastFoodLevel;
+
+    @Shadow private int foodLevel;
+
     @Inject(
             method = "tick",
             at = @At("HEAD"),
@@ -31,6 +37,22 @@ public class HungerManagerMixin {
     public void setHunger(Player player, CallbackInfo ci) {
         if (GamePhaseManager.INSTANCE.isNotStarted()) {
             ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At("TAIL")
+    )
+    public void lessHunger(Player player, CallbackInfo ci) {
+        int diff = lastFoodLevel - foodLevel;
+        if (diff <= 0) return;
+
+        boolean applicable = diff == 1;
+        double hungerRate = 0.15;
+
+        if (Random.Default.nextDouble() > hungerRate && applicable) {
+            foodLevel = lastFoodLevel;
         }
     }
 }
