eb42bdd1a5a651cb057cb9f0530da552777ca5e2
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-12-09 00:24:31 +0100

    ende

--- src/main/kotlin/de/royzer/fabrichg/game/phase/phases/EndPhase.kt
+++ src/main/kotlin/de/royzer/fabrichg/game/phase/phases/EndPhase.kt
@@ -11,13 +11,28 @@ import de.royzer.fabrichg.game.phase.GamePhase
 import de.royzer.fabrichg.game.phase.PhaseType
 import de.royzer.fabrichg.proxy.ProxyManager
 import de.royzer.fabrichg.proxyManager
+import de.royzer.fabrichg.server
+import it.unimi.dsi.fastutil.ints.IntArrayList
+import net.minecraft.core.BlockPos
+import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.HoverEvent
 import net.minecraft.network.protocol.game.ClientboundPlayerAbilitiesPacket
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.projectile.FireworkRocketEntity
+import net.minecraft.world.item.FireworkRocketItem
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.FireworkExplosion
+import net.minecraft.world.item.component.Fireworks
+import net.minecraft.world.level.block.Blocks
+import net.silkmc.silk.core.item.itemStack
 import net.silkmc.silk.core.logging.logInfo
+import net.silkmc.silk.core.task.mcCoroutineTask
 import net.silkmc.silk.core.text.literalText
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 class EndPhase(private val winner: HGPlayer?) : GamePhase() {
 
@@ -37,6 +52,65 @@ class EndPhase(private val winner: HGPlayer?) : GamePhase() {
         }
         winner.updateStats(wins = 1)
         proxyManager.sendStatus(ProxyManager.ServerStatus.UNREACHABLE)
+
+        placeKuchen()
+
+        server.playerList.players.forEach { player ->
+            player.teleportTo(0.0, 101.0, 0.0)
+        }
+    }
+
+    fun placeKuchen() {
+        val size = 3
+
+        val starExplosion = FireworkExplosion(
+            FireworkExplosion.Shape.STAR,
+            IntArrayList(listOf(0xFF0000)),
+            IntArrayList(listOf(0xFF0000)),
+            true,
+            true
+        )
+        val creeperExplosion = FireworkExplosion(
+            FireworkExplosion.Shape.CREEPER,
+            IntArrayList(listOf(0x00FF00)),
+            IntArrayList(listOf(0x00FF00)),
+            true,
+            true
+        )
+        val burstExplosion = FireworkExplosion(
+            FireworkExplosion.Shape.BURST,
+            IntArrayList(listOf(0x0000FF)),
+            IntArrayList(listOf(0x0000FF)),
+            true,
+            true
+        )
+
+        val explosions = listOf(starExplosion, creeperExplosion, burstExplosion)
+
+
+        val player = winner?.serverPlayer ?: return
+
+        mcCoroutineTask(howOften = 50L, period = .1.seconds) { task ->
+            for (x in -size..size) {
+                for (z in -size..size) {
+                    player.level()?.setBlockAndUpdate(BlockPos(x, 100, z), Blocks.CAKE.defaultBlockState())
+
+                    val fireworkStack = itemStack(Items.FIREWORK_ROCKET) {
+                        set(DataComponents.FIREWORKS, Fireworks(Random.nextInt(1, 3), listOf(explosions.random())))
+                    }
+
+                    val fireworkRocketEntity = FireworkRocketEntity(
+                        player.level(),
+                        player,
+                        x.toDouble(),
+                        100.0,
+                        z.toDouble(),
+                        fireworkStack.copy()
+                    );
+                    player.level().addFreshEntity(fireworkRocketEntity);
+                }
+            }
+        }
     }
 
     override fun tick(timer: Int) {
