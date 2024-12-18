ea1f1aa90f80de5e438a81fe55100222edc5319c
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-12-18 23:40:21 +0100

    gulag fix aber ohne test

--- src/main/kotlin/de/royzer/fabrichg/mixinskt/LivingEntityMixinKt.kt
+++ src/main/kotlin/de/royzer/fabrichg/mixinskt/LivingEntityMixinKt.kt
@@ -13,13 +13,12 @@ import net.silkmc.silk.core.task.mcCoroutineTask
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
 object LivingEntityMixinKt {
-    fun onDamage(source: DamageSource, amount: Float, entity: LivingEntity, cir: CallbackInfoReturnable<Boolean>) {
+    fun canDamage(source: DamageSource, entity: LivingEntity): Boolean {
         val level = entity.level()
 
         if (level == GulagManager.gulagLevel) {
-            if (source.type() == DamageTypes.GENERIC_KILL) return
-
-            if (entity !is ServerPlayer) return
+            if (source.type() == DamageTypes.GENERIC_KILL) return true
+            if (entity !is ServerPlayer) return true
 
             val sourceEntity = source.entity
 
@@ -27,14 +26,23 @@ object LivingEntityMixinKt {
             val fighting = GulagManager.isFighting(entity)
 
             if (!sourceEntityFighting || !fighting) {
-                cir.returnValue = false
-                return
+                return false
             }
         }
 
         if (entity is ServerPlayer || entity is HGBot) {
-            if (GamePhaseManager.currentPhaseType != PhaseType.INGAME) cir.returnValue = false
+            if (GamePhaseManager.currentPhaseType != PhaseType.INGAME) return false
         } else if (GamePhaseManager.currentPhaseType == PhaseType.LOBBY || GamePhaseManager.currentPhaseType == PhaseType.END)
+            return false
+
+        return true
+
+    }
+
+    fun onDamage(source: DamageSource, amount: Float, entity: LivingEntity, cir: CallbackInfoReturnable<Boolean>) {
+        if (!canDamage(source, entity)) {
             cir.returnValue = false
+            cir.cancel()
+        }
     }
 }
