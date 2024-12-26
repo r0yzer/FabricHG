64814fb8549d0a96652169ad6e74bba662f805c5
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-12-26 23:23:11 +0100

    irgendwas geht safe nichtmehr

---
+++ src/main/kotlin/de/royzer/fabrichg/kit/kits/BerserkerKit.kt
@@ -0,0 +1,43 @@
+package de.royzer.fabrichg.kit.kits
+
+import de.royzer.fabrichg.data.hgplayer.hgPlayer
+import de.royzer.fabrichg.kit.achievements.delegate.achievement
+import de.royzer.fabrichg.kit.kit
+import de.royzer.fabrichg.kit.property.property
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.item.Items
+
+val berserkerKit = kit("Berserker") {
+    kitSelectorItem = Items.BLAZE_POWDER.defaultInstance
+
+    description = "kill entities to gain strength"
+
+    val speedAmplifier by property(2, "speed amplifier")
+    val strengthAmplifier by property(2, "strength amplifier")
+
+    val entityKillEffectTime by property(2, "entity kill efect time")
+    val playerKillEffectTime by property(10, "player kill efect time")
+
+    val berserkSecondsAchievement by achievement("berserk seconds") {
+        level(60)
+        level(300)
+        level(1000)
+    }
+
+    kitEvents {
+        onKillEntity { hgPlayer, kit, entity ->
+            val serverPlayer = hgPlayer.serverPlayer ?: return@onKillEntity
+
+            val berserkerTime = if (entity.hgPlayer == null) entityKillEffectTime else playerKillEffectTime
+
+            serverPlayer.addEffect(MobEffectInstance(MobEffects.MOVEMENT_SPEED, 20 * berserkerTime, speedAmplifier))
+            serverPlayer.addEffect(MobEffectInstance(MobEffects.DAMAGE_BOOST, 20 * berserkerTime, strengthAmplifier))
+
+            berserkSecondsAchievement.awardLater(serverPlayer, berserkerTime)
+
+            serverPlayer.playSound(SoundEvents.WOLF_HOWL, 10f, 1f)
+        }
+    }
+}
