d5d95bad184dc3445cf0af9c66fb8d04a291f70e
jakob <royzerfisch@gmail.com>
2025-01-22 22:46:07 +0100

    pacifist check

--- src/main/kotlin/de/royzer/fabrichg/kit/kits/PacifistKit.kt
+++ src/main/kotlin/de/royzer/fabrichg/kit/kits/PacifistKit.kt
@@ -4,5 +4,6 @@ import de.royzer.fabrichg.kit.kit
 import net.minecraft.world.item.Items
 
 val pacifistKit = kit("Pacifist") {
+    description = "Take and deal only 3/4 damage"
     kitSelectorItem = Items.DANDELION.defaultInstance
 }
