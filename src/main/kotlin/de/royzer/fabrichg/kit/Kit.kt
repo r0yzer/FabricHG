061e09a18677e58104e49c6e1422d1cec76164e3
jakob <royzerfisch@gmail.com>
2025-01-22 22:19:14 +0100

    pacifist

--- src/main/kotlin/de/royzer/fabrichg/kit/Kit.kt
+++ src/main/kotlin/de/royzer/fabrichg/kit/Kit.kt
@@ -152,6 +152,7 @@ val kits = listOfNotNull(
     minerKit,
     copycatKit,
 //    goatKit,
+    pacifistKit,
 )
 
 /**
---
+++ src/main/kotlin/de/royzer/fabrichg/kit/kits/PacifistKit.kt
@@ -0,0 +1,8 @@
+package de.royzer.fabrichg.kit.kits
+
+import de.royzer.fabrichg.kit.kit
+import net.minecraft.world.item.Items
+
+val pacifistKit = kit("Pacifist") {
+    kitSelectorItem = Items.DANDELION.defaultInstance
+}
